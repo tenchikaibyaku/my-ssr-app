04dfc4d3c9c8474656cda04d8ec4761b515fe4ae
Kumagai <obama0419@gmail.com>
2025-02-27 08:31:55 +0900

    作業04を修正2

--- renderer/_default.page.server.tsx
+++ renderer/_default.page.server.tsx
@@ -23,7 +23,7 @@ async function render(pageContext: PageContext) {
     <head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>SSR React App</title>
+      <title>鴨川湯 紹介ページ</title>
     </head>
     <body>
       <div id="app">${dangerouslySkipEscape(pageHtml)}</div>
