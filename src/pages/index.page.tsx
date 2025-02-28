5ac91527a134e799875c36b403e94e3cc8e5c3c3
Kumagai <obama0419@gmail.com>
2025-02-28 13:35:30 +0900

    作業01を修正6

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,8 +1,8 @@
-function Page({ message }: { message?: string }) {
+function Page() {
   return (
     <div>
       <h1>鴨川湯 - Kyoto Sento</h1>
-      <p>{message ?? 'ようこそ！'}</p>
+      <p>ようこそ！</p>
     </div>
   );
 }
