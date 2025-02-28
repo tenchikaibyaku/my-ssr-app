d670e73ac97d1be544c5d6b9c85538fb5ca3d0aa
Kumagai <obama0419@gmail.com>
2025-02-28 13:34:03 +0900

    Updates

---
+++ assets/entries/src_pages_index.page.Dz4y5ZSs.js
@@ -0,0 +1,1 @@
+import{j as r}from"../chunks/chunk-D_zvdyIk.js";function n(){return r.jsxs("div",{children:[r.jsx("h1",{children:"鴨川湯 - Kyoto Sento"}),r.jsx("p",{children:"ようこそ！"})]})}export{n as Page};
